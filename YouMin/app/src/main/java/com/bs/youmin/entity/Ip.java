f82fa5abc37135fe021b7af987e6640e120c7186
zwj1994 <767899412@qq.com>
2018-03-27 13:19:04 +0800

    Ip.java还原

--- YouMin/app/src/main/java/com/bs/youmin/entity/Ip.java
+++ YouMin/app/src/main/java/com/bs/youmin/entity/Ip.java
@@ -10,6 +10,5 @@ public class Ip {
      * 服务器地址
      */
     public static final String SERVER_URL = "http://192.168.0.130:8080/";
-    public static final String SERVER_URL1 = "http://192.168.0.130:8080/";
-    public static final String SERVER_URL2 = "http://192.168.0.130:8080/";
+
 }
