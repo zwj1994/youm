078bb5a60a7968620a25bc206db79c954f77a891
zwj1994 <767899412@qq.com>
2018-03-28 16:13:17 +0800

    Ip文件修改

--- YouMin/app/src/main/java/com/bs/youmin/entity/Ip.java
+++ YouMin/app/src/main/java/com/bs/youmin/entity/Ip.java
@@ -9,6 +9,7 @@ public class Ip {
     /**
      * 服务器地址
      */
-    public static final String SERVER_URL = "http://192.168.5.22:8080/";
+//    public static final String SERVER_URL = "http://192.168.5.22:8080/";
+    public static final String SERVER_URL = "http://www.zuvips.com:8080/youm/";
 
 }
