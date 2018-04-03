ac99f9fec9b0655fae675df0a8d59cc193db35da
zwj1994 <767899412@qq.com>
2018-04-03 21:53:02 +0800

    新增与修改

--- YouMin/app/src/main/java/com/bs/youmin/fragment/AlbumRankingFragment.java
+++ YouMin/app/src/main/java/com/bs/youmin/fragment/AlbumRankingFragment.java
@@ -1,17 +1,22 @@
 package com.bs.youmin.fragment;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bs.youmin.R;
@@ -39,7 +44,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  *  创建时间:  2018/03/23 19:48
  *  描述：    相册排行
  */
-public class AlbumRankingFragment extends Fragment {
+public class AlbumRankingFragment extends Fragment implements View.OnClickListener{
 
     private ApiImp apiImp;
 
@@ -51,6 +56,21 @@ public class AlbumRankingFragment extends Fragment {
     private TextView ranking_load_more_tv;
     private int page = 0;
 
+
+    private FloatingActionButton fab01Add;
+    private boolean isAdd = false;
+    private RelativeLayout rlAddBill;
+    private int[] llId = new int[]{R.id.ll01,R.id.ll02,R.id.ll03};
+    private LinearLayout[] ll = new LinearLayout[llId.length];
+    private int[] fabId = new int[]{R.id.miniFab01,R.id.miniFab02,R.id.miniFab03};
+    private FloatingActionButton[] fab = new FloatingActionButton[fabId.length];
+    private AnimatorSet addBillTranslate1;
+    private AnimatorSet addBillTranslate2;
+    private AnimatorSet addBillTranslate3;
+    private AnimatorSet addBillTranslate4;
+    private AnimatorSet addBillTranslate5;
+    private AnimatorSet addBillTranslate6;
+
     private Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
@@ -58,14 +78,32 @@ public class AlbumRankingFragment extends Fragment {
         }
     };
 
+    public AlbumRankingFragment() {
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_album_ranking, container, false);
         initView(view);
+
+        setDefaultValues();
+        bindEvents();
         return view;
     }
 
     private void initView(View view) {
+
+        fab01Add = (FloatingActionButton)view.findViewById(R.id.fab01Add);
+        rlAddBill = (RelativeLayout)view.findViewById(R.id.rlAddBill);
+        rlAddBill.setOnClickListener(this);
+
+        for (int i = 0; i < llId.length;i++){
+            ll[i] = (LinearLayout)view.findViewById(llId[i]);
+        }
+        for (int i = 0;i < fabId.length; i++){
+            fab[i] = (FloatingActionButton)view.findViewById(fabId[i]);
+        }
+
         load_more = (LinearLayout) view.findViewById(R.id.ranking_load_more);
         ranking_load_more_tv = (TextView) view.findViewById(R.id.ranking_load_more_tv);
         mListView = (ListView) view.findViewById(R.id.mListView);
@@ -147,5 +185,58 @@ public class AlbumRankingFragment extends Fragment {
         mList.addAll(list);
         albumRankingListAdapter = new AlbumRankingListAdapter(getActivity(), mList);
         mListView.setAdapter(albumRankingListAdapter);
+    }
+
+
+
+    private void setDefaultValues(){
+        addBillTranslate1 = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(),R.anim.add_bill_anim);
+        addBillTranslate2 = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(),R.anim.add_bill_anim);
+        addBillTranslate3 = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(),R.anim.add_bill_anim);
+    }
+    private void bindEvents(){
+        fab01Add.setOnClickListener(this);
+        for (int i = 0;i < fabId.length; i++){
+            fab[i].setOnClickListener(this);
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rlAddBill:
+                hideFABMenu();
+                break;
+            case R.id.fab01Add:
+                fab01Add.setImageResource(isAdd ? R.drawable.icon_add_btn:R.drawable.icon_add_click_btn);
+                isAdd = !isAdd;
+                rlAddBill.setVisibility(isAdd ? View.VISIBLE : View.GONE);
+                if (isAdd) {
+                    addBillTranslate1.setTarget(ll[0]);
+                    addBillTranslate1.start();
+                    addBillTranslate2.setTarget(ll[1]);
+                    addBillTranslate2.setStartDelay(150);
+                    addBillTranslate2.start();
+                    addBillTranslate3.setTarget(ll[2]);
+                    addBillTranslate3.setStartDelay(200);
+                    addBillTranslate3.start();
+                }
+                break;
+            case R.id.miniFab01:
+                hideFABMenu();
+                break;
+            case R.id.miniFab02:
+                hideFABMenu();
+                break;
+            case R.id.miniFab03:
+                hideFABMenu();
+                break;
+            default:
+                break;
+        }
+    }
+    private void hideFABMenu(){
+        rlAddBill.setVisibility(View.GONE);
+        fab01Add.setImageResource(R.drawable.icon_add_btn);
+        isAdd = false;
     }
 }
